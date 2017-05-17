023518219b041b6c006fe877dedd93ddf1cd66b8
Nicolás <nicolas.js@openmailbox.org>
2017-05-17 01:37:31 -0300

    Add files via upload

--- P01.Ejer. Completos/ej.01_P1 2017 - MaxMinPascal.pas
+++ P01.Ejer. Completos/ej.01_P1 2017 - MaxMinPascal.pas
@@ -1,22 +1,25 @@
-{1.Realizar un programa que lea 2 números enteros desde teclado e 
-informe en pantalla cuál de los dos números es el mayor. 
+{1.Realizar un programa que lea 2 nÃºmeros enteros desde teclado e 
+informe en pantalla cuÃ¡l de los dos nÃºmeros es el mayor. 
 Si son iguales debe informar en pantalla lo siguiente: 
-“Los números leídos son iguales”.}
+â€œLos nÃºmeros leÃ­dos son igualesâ€.}
 
-program Prac1.Ej1
+program Ej1;
 var
-	num1,num2 : integer;
-	num.max : real;
+   n1, n2 : integer;
+   max : real;
 begin
-	Read(num1);
-	Read(num2);
-	if (num1 > num2) then begin
-		num.max := num1;
-		writeln ("El numero maximo es: " ,num.max)
-		if (num2 > num1) then
-			num.max := num2
-			writeln ("El numero maximo es: " ,num.max)
-		else
-			writeln ("Los numero leídos son	iguales”")
-		end
-		end.
+     readln(n1);
+     readln(n2);
+     if (n1 > n2) then begin
+        max := n1;
+        writeln('El numero mas grande es: ', max:2:2);
+     end;
+     if (n2 > n1) then begin
+        max:= n2;
+        writeln('El numero mas grande es: ', max:2:2);
+     end;
+     if (n2 = n1) then begin
+        writeln('Los numeros ingresados son iguales');
+     end;
+        readln;
+end.
